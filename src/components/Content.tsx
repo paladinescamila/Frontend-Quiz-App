4be39c389d7d071d100796118917ad197ace82c6
Camila <camilapaladines27@gmail.com>
2026-03-30 16:55:45 -0500

    Fix

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -13,14 +13,13 @@ export default function Content({
 	return (
 		<main className='flex flex-col gap-21 bg-grey-50 dark:bg-blue-900 px-6 xl:px-16 2xl:px-53 p-6 md:py-14 xl:py-24 background-light-mobile dark:background-dark-mobile md:background-light-tablet md:dark:background-dark-tablet lg:background-light-desktop lg:dark:background-dark-desktop w-screen h-screen'>
 			<section className='flex flex-row items-center justify-between gap-4'>
-				{subject ? (
-					<div className='flex flex-row gap-4 md:gap-6 items-center'>
-						<SubjectIcon subject={subject} />
-						<h1 className='text-preset-4-mobile md:text-preset-4 text-blue-900 dark:text-white'>
-							{QUIZES_BY_SUBJECT[subject].title}
-						</h1>
-					</div>
-				) : null}
+				<div
+					className={`flex flex-row gap-4 md:gap-6 items-center ${subject ? '' : 'opacity-0 pointer-events-none'}`}>
+					<SubjectIcon subject={subject || 'html'} />
+					<h1 className='text-preset-4-mobile md:text-preset-4 text-blue-900 dark:text-white'>
+						{QUIZES_BY_SUBJECT[subject || 'html'].title}
+					</h1>
+				</div>
 				<ThemeToggle />
 			</section>
 			<section className='grid grid-rows-[auto_1fr] lg:grid-cols-[456px_1fr] gap-10 md:gap-16 xl:gap-32'>
